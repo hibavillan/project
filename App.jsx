18e7df0d105b15eb765e7f370770fd2c3bdb096d
Hiba Noushad Villan <hibanoushadvillan@gmail.com>
2024-08-02 22:29:31 -0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,21 +3,22 @@ import './App.css'
 import Home from './components/Home'
 import Login from './components/Login'
 import { Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-
-
-
+import Navbar2 from './components/Navbar'
+import AddRecipes from './components/AddRecipes'
+import Recipes from './components/Recipes'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <Navbar/>
+   <Navbar2/>
+   
      <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/C' element={<Login/>} />
-  
+          <Route path='/B' element={<AddRecipes/>} />
+          <Route path='/recipes' element={<Recipes/>} />
         </Routes>
      </>
   )
